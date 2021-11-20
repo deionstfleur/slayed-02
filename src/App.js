b2faaff7377fd4c8baea3f26ecf2f23050b05993
Deion Stfleur <deionstfleur@Deions-MacBook-Pro.local>
2021-11-20 08:46:13 -0500

    saturday

--- src/App.js
+++ src/App.js
@@ -53,106 +53,12 @@ function App() {
         </div>
     </nav>
 
-    
-
-
-
-    {/* <div  class="position-relative overflow-hidden p-3 p-md-5 text-center bg">
-      <div class="col-md-5 p-lg-5 mx-auto my-5">
-        <h1 style={{paddingTop: 33}} class="display-4 font-weight-normal">Slayed by ...</h1>
-        <p class="lead font-weight-normal">And an even wittier subheading to boot. Jumpstart your marketing efforts with this example based on Apple's marketing pages.</p>
-        <a class="btn btn-outline-secondary" href="#socials-nav">Learn More</a>
-      </div>
-      <div class="product-device box-shadow d-none d-md-block"></div>
-      <div class="product-device product-device-2 box-shadow d-none d-md-block"></div>
-    </div> */}
-
-
     <div>
     <div>
     <Landing />
-      {/* <h1 class="text-center" id="quick-fix">Key features of our services website templates</h1> */}
 
-{/* <div class="container">
-      <div class="row mb-2">
-        <div class="col-md-6">
-          <div class="card flex-md-row mb-4 box-shadow h-md-250">
-            <div class="card-body d-flex flex-column align-items-start">
-              <strong class="d-inline-block mb-2 text-primary">World</strong>
-              <h3 class="mb-0">
-                <a class="text-dark" href="#">Featured post</a>
-              </h3>
-              <div class="mb-1 text-muted">Nov 12</div>
-              <p class="card-text mb-auto">This is a wider card with supporting text below as a natural lead-in to additional content.</p>
-              <a href="#">Continue reading</a>
-            </div>
-            <img class="card-img-right flex-auto d-none d-md-block" data-src="holder.js/200x250?theme=thumb" alt="Thumbnail [200x250]" style={{width: 200, height: 250}} src="data:image/svg+xml;charset=UTF-8,%3Csvg%20width%3D%22200%22%20height%3D%22250%22%20xmlns%3D%22http%3A%2F%2Fwww.w3.org%2F2000%2Fsvg%22%20viewBox%3D%220%200%20200%20250%22%20preserveAspectRatio%3D%22none%22%3E%3Cdefs%3E%3Cstyle%20type%3D%22text%2Fcss%22%3E%23holder_17d2c2ba25e%20text%20%7B%20fill%3A%23eceeef%3Bfont-weight%3Abold%3Bfont-family%3AArial%2C%20Helvetica%2C%20Open%20Sans%2C%20sans-serif%2C%20monospace%3Bfont-size%3A13pt%20%7D%20%3C%2Fstyle%3E%3C%2Fdefs%3E%3Cg%20id%3D%22holder_17d2c2ba25e%22%3E%3Crect%20width%3D%22200%22%20height%3D%22250%22%20fill%3D%22%2355595c%22%3E%3C%2Frect%3E%3Cg%3E%3Ctext%20x%3D%2256.1953125%22%20y%3D%22130.7%22%3EThumbnail%3C%2Ftext%3E%3C%2Fg%3E%3C%2Fg%3E%3C%2Fsvg%3E" data-holder-rendered="true" />
-          </div>
-        </div>
-        <div class="col-md-6">
-          <div class="card flex-md-row mb-4 box-shadow h-md-250">
-            <div class="card-body d-flex flex-column align-items-start">
-              <strong class="d-inline-block mb-2 text-success">Design</strong>
-              <h3 class="mb-0">
-                <a class="text-dark" href="#">Post title</a>
-              </h3>
-              <div class="mb-1 text-muted">Nov 11</div>
-              <p class="card-text mb-auto">This is a wider card with supporting text below as a natural lead-in to additional content.</p>
-              <a href="#">Continue reading</a>
-            </div>
-            <img class="card-img-right flex-auto d-none d-md-block" data-src="holder.js/200x250?theme=thumb" alt="Thumbnail [200x250]" src="data:image/svg+xml;charset=UTF-8,%3Csvg%20width%3D%22200%22%20height%3D%22250%22%20xmlns%3D%22http%3A%2F%2Fwww.w3.org%2F2000%2Fsvg%22%20viewBox%3D%220%200%20200%20250%22%20preserveAspectRatio%3D%22none%22%3E%3Cdefs%3E%3Cstyle%20type%3D%22text%2Fcss%22%3E%23holder_17d2c2ba260%20text%20%7B%20fill%3A%23eceeef%3Bfont-weight%3Abold%3Bfont-family%3AArial%2C%20Helvetica%2C%20Open%20Sans%2C%20sans-serif%2C%20monospace%3Bfont-size%3A13pt%20%7D%20%3C%2Fstyle%3E%3C%2Fdefs%3E%3Cg%20id%3D%22holder_17d2c2ba260%22%3E%3Crect%20width%3D%22200%22%20height%3D%22250%22%20fill%3D%22%2355595c%22%3E%3C%2Frect%3E%3Cg%3E%3Ctext%20x%3D%2256.1953125%22%20y%3D%22130.7%22%3EThumbnail%3C%2Ftext%3E%3C%2Fg%3E%3C%2Fg%3E%3C%2Fsvg%3E" data-holder-rendered="true" style={{width: 200, height: 250}} />
-          </div>
-        </div>
-      </div>
 
-</div> */}
-    
-{/* <div style={{backgroundColor: 'lightblue'}} >
-      <div class="container">
-        
-        <div class="row">
-
-          <div class="col-md-6">
-            <h2 class="title">contact !</h2>
-          </div>
-
-          <div class="col-md-6">
-            <div class="row">
-
-              <div class="col-md-3">
-                <h1>+</h1>
-                <p>lorem</p>
-              </div>
-
-              <div class="col-md-3">
-                <h1>+</h1>
-                <p>lorem</p>
-              </div>
-
-              <div class="col-md-3">
-                <h1>+</h1>
-                <p>lorem</p>
-              </div>
-
-              <div class="col-md-3">
-                <h1>+</h1>
-                <p>lorem</p>
-              </div>
-
-            </div>
-          </div>
-
-        </div>
-
-      </div>
-
-</div> */}
-
-{/* <div>
-  <h2 id="title-two" class="text-center">Social Media</h2>
-</div> */}
-
-<div id="section" class="container" style={{paddingTop: 84, marginBottom: 84}}>
+<div class="container section-1">
         {/* <!-- Example row of columns --> */}
         <div class="row text-center">
           <div class="col-md-6">
